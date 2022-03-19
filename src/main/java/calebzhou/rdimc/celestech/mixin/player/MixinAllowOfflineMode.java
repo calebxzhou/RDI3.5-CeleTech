e804715babf0308f52333b293e5c5852aaafb6ac
calebz <portableryanc69@outlook.com>
2022-03-19 21:34:34 +0800

    123

--- src/main/java/calebzhou/rdimc/celestech/mixin/player/MixinAllowOfflineMode.java
+++ src/main/java/calebzhou/rdimc/celestech/mixin/player/MixinAllowOfflineMode.java
@@ -2,31 +2,48 @@ package calebzhou.rdimc.celestech.mixin.player;
 
 
 import calebzhou.rdimc.celestech.RDICeleTech;
+import calebzhou.rdimc.celestech.module.Auth;
 import calebzhou.rdimc.celestech.utils.HttpUtils;
+import calebzhou.rdimc.celestech.utils.IpUtils;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.mojang.authlib.GameProfile;
 import net.minecraft.DefaultUncaughtExceptionHandler;
 import net.minecraft.network.Connection;
+import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.login.ClientboundHelloPacket;
+import net.minecraft.network.protocol.login.ServerboundHelloPacket;
 import net.minecraft.server.network.ServerLoginPacketListenerImpl;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.Shadow;
+import org.apache.commons.lang3.Validate;
+import org.checkerframework.checker.units.qual.A;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.net.InetSocketAddress;
 
 //@Mixin(ServerConfigHandler.class)
 @Mixin(ServerLoginPacketListenerImpl.class)
 public abstract class MixinAllowOfflineMode{
-    @Shadow @Final public Connection connection;
-    @Shadow @Mutable
-    GameProfile gameProfile;
-    @Shadow @Mutable
-    ServerLoginPacketListenerImpl.State state;
 
-    @Redirect(
+    @Shadow @Final public Connection connection;
+
+    @Inject(method = "Lnet/minecraft/server/network/ServerLoginPacketListenerImpl;handleHello(Lnet/minecraft/network/protocol/login/ServerboundHelloPacket;)V",
+    at = @At("TAIL"))
+    private void checkAllowLogin(ServerboundHelloPacket serverboundHelloPacket, CallbackInfo ci){
+        Auth.handleHello((ServerLoginPacketListenerImpl)(Object)this,serverboundHelloPacket);
+    }
+    /*@Overwrite
+    public void handleHello(ServerboundHelloPacket serverboundHelloPacket) {
+        Validate.validState(this.state == ServerLoginPacketListenerImpl.State.HELLO, "Unexpected hello packet", new Object[0]);
+        Auth.handleHello((ServerLoginPacketListenerImpl)(Object)this,serverboundHelloPacket);
+        this.gameProfile = new GameProfile(Auth.getNewUuid(gameProfile.getName()),gameProfile.getName());
+        this.state = ServerLoginPacketListenerImpl.State.READY_TO_ACCEPT;
+    }*/
+    /*@Redirect(
             method = "Lnet/minecraft/server/network/ServerLoginPacketListenerImpl;handleHello(Lnet/minecraft/network/protocol/login/ServerboundHelloPacket;)V",
             at=@At(
                     target = "Lnet/minecraft/network/Connection;send(Lnet/minecraft/network/protocol/Packet;)V",
@@ -80,7 +97,7 @@ public abstract class MixinAllowOfflineMode{
         });
         thread.setUncaughtExceptionHandler(new DefaultUncaughtExceptionHandler(RDICeleTech.LOGGER));
         thread.start();
-    }
+    }*/
     @Shadow
     protected abstract GameProfile createFakeProfile(GameProfile profile);
 
---
+++ src/main/java/calebzhou/rdimc/celestech/module/Auth.java
@@ -0,0 +1,30 @@
+package calebzhou.rdimc.celestech.module;
+
+import calebzhou.rdimc.celestech.utils.HttpUtils;
+import calebzhou.rdimc.celestech.utils.IpUtils;
+import com.mojang.authlib.GameProfile;
+import net.minecraft.network.Connection;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.protocol.login.ServerLoginPacketListener;
+import net.minecraft.network.protocol.login.ServerboundHelloPacket;
+import net.minecraft.server.network.ServerLoginPacketListenerImpl;
+
+import java.lang.reflect.Field;
+import java.net.InetSocketAddress;
+import java.util.UUID;
+
+public class Auth {
+    public static void handleHello(ServerLoginPacketListenerImpl listener, ServerboundHelloPacket packet) {
+        String usr = packet.getGameProfile().getName();
+        String qq = HttpUtils.sendRequestRaw("GET","account_utils","method=rdi2qq","param="+usr);
+        byte[] ip = ((InetSocketAddress)listener.getConnection().getRemoteAddress()).getAddress().getAddress();
+        String ip10 = ((InetSocketAddress)listener.getConnection().getRemoteAddress()).getHostString();
+        String response = HttpUtils.sendRequestRaw("GET", "account_login", "usr=" + usr,"ip="+IpUtils.ip2addr(ip),"ip10="+ip10);
+        //登录不成功
+        if(!response.equals("true")){
+            String reason = "请您登录QQ "+qq.charAt(0)+"********"+qq.charAt(qq.length()-1)+"，向RDI机器人发送： r#login="+ IpUtils.ip2addr(ip)+
+                    "\n发送成功后，重新连接服务器。";
+            listener.disconnect(new TextComponent(reason));
+        }
+    }
+}
